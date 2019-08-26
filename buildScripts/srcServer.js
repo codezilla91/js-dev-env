983939a1d33e335a9231c01c1f15f686c0135930
Leon Matota <leonmatota@gmail.com>
2019-08-26 09:32:46 +0200

    added mock api

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -11,11 +11,19 @@ const compiler = webpack(config);
 app.use(require('webpack-dev-middleware')(compiler, {
   noInfo: true,
   publicPath: config.output.publicPath
-}))
+}));
 
 app.get('/', function(req, resp) {
   resp.sendFile(path.join(__dirname, '../src/index.html'));
 });
+
+app.get('/users', function(req, res) {
+    res.json([
+        {"id": 1,"firstName": "Leon", "lastName": "Matota", "email": "leonmatota@gmail.com"},
+        {"id": 2, "firstName": "Tammy", "lastName": "Norton", "email": "tnorton@yahoo.com"},
+        {"id": 3, "firstName": "Tina", "lastName": "Lee", "email": "lee.tina@hotmail.com"}
+    ])
+})
 
 app.listen(port, function(err) {
   if (err) {
